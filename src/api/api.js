9561e701968fb24b8dd87faee962d663689e226d
abhigupta0507 <abhishekgupta39585@gmail.com>
2025-09-19 01:27:37 +0530

    fixed deployment

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 // API Configuration
-const API_BASE_URL = process.env.API_BASE_URL;
-
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URl;
+console.log(process.env);
 // API Helper Functions
 export const api = {
   login: async (email, password) => {
